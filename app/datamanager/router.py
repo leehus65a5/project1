d4e314e58dd143dce6db6726c466f54cd8249ddf
camap2002 <vannguyen26032002@gmail.com>
2022-12-25 07:27:05 +0700

    van commit

--- app/datamanager/router.py
+++ app/datamanager/router.py
@@ -1,14 +1,15 @@
-from app import app, db
-from app.model import Files, A10, Files2, FileLog
+from app import db, mySql, app, tools
+from flask import render_template, url_for, flash, redirect, g, request, jsonify, session, send_file
 from app.datamanager import datamanager
+from app.model import A10, Udata, Files2, FileLog
+from sqlalchemy import select, and_
 from app.form import UpLoadForm, DownloadForm
-from flask import render_template, url_for, flash, redirect, g, request, send_file
+from app import tools
 from werkzeug.utils import secure_filename
-import os
+import csv, os
 from io import BytesIO
 from sqlalchemy import select, and_
-from app import tools
-from datetime import datetime
+
 
 @datamanager.before_request
 def check_auth():
@@ -19,7 +20,7 @@ def check_auth():
 
 @datamanager.route('/dashboard')
 def dashboard():
-     return render_template('datamanager/dashboard.html')
+     return render_template('datamanager/base.html')
 
 @datamanager.route('/uploadfile', methods = ['GET', 'POST'])
 def uploadfile():
@@ -104,3 +105,67 @@ def downloadfile():
 @datamanager.route('/test', methods = ['GET','POST'])
 def test():
      return render_template('datamanager/test.html')
+
+@datamanager.route('/plot')
+def plot():
+     return render_template('datamanager/plotdata.html')
+
+
+@datamanager.route('/check', methods=['GET', 'POST'])
+def check():
+     uid = session.get('user_id')
+     listTable = db.session.execute(select(Udata.tableid).where(Udata.userid == uid))
+     listTable = [i.tableid for i in listTable]
+     
+     data = None
+     listk = None
+     form = request.form
+     check_table = session.get('table')
+     
+     print('check table', check_table)
+     
+     if 'name' not in form and not check_table:
+          print('no name and check table')
+          return render_template('datamanager/data.html', datas = None, listkey = None)
+     print('here')
+     
+     
+     if request.method == 'POST':
+          if 'name' in form and len(form['name']) >= 1:
+               session['table'] = form['name']
+               tb_cls = db.Table(session['table'], db.metadata,autoload=True,autoload_with=db.engine)
+               data = db.session.execute(select(tb_cls)).fetchall()
+               listk = [i.name for i in tb_cls.columns]
+               listk = sorted(listk)
+               print('check1')
+               print(session['table'])
+               # return render_template('user/check.html', datas = data, listkey = listk)
+          elif 'start' in form:
+               print('check 2 name not in table, get selected collums')
+               print(form)
+               listk = [i for i in form if i not in ['start', 'stop']]
+               print(listk)
+               tb_cls = db.Table(session['table'], db.metadata,autoload=True,autoload_with=db.engine)
+               sql = select(*[getattr(tb_cls.c, i) for i in listk]).where(and_(tb_cls.c.DEPT >= form['start'], tb_cls.c.DEPT <= form['stop']))
+               data = db.session.execute(sql).fetchall()
+               print('check2')
+               # return render_template('user/check.html', datas = data, listkey = listk)
+          else:
+               tb_cls = db.Table(session['table'], db.metadata,autoload=True,autoload_with=db.engine)
+               data = db.session.execute(select(tb_cls)).fetchall()
+               listk = [i.name for i in tb_cls.columns]
+               listk = sorted(listk)
+               print(listk)
+               print('check3')
+               # return render_template('user/check.html', datas = data, listkey = listk)
+          
+          print('check4')
+          
+     # a10 = db.Table('a10', db.metadata,autoload=True,autoload_with=db.engine)
+     # sql = select(a10)
+     # r = db.session.execute(sql).fetchall()
+     # for i in r:
+     #      print(i)
+     
+     print('chech5')
+     return render_template('datamanager/data.html', datas = data, listkey = listk, listTable = listTable)
